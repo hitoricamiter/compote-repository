d75d8718afece7c258d664329a500ade77bfc78e
hitoricamiter <koshurnikovaslyam@mail.ru>
2025-07-12 19:57:29 +0600

    initialize project with dependencies

---
+++ src/main/java/ru/zaikin/compote_microservices/CompoteMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package ru.zaikin.compote_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompoteMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompoteMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/zaikin/compote_microservices/CompoteMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zaikin.compote_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompoteMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
